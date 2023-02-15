bb5151b98b8b13c0ac5d01c98126cab5d82f7a50
Irving78rus <isakov911@mail.ru>
2023-02-16 01:33:03 +0300

    script for HH

---
+++ HHScript.js
@@ -0,0 +1,28 @@
+(() => {
+	let delay = 0;
+	const jobs = document.querySelectorAll('[data-qa="vacancy-serp__vacancy_response"]');
+	jobs.forEach(job => {
+		setTimeout(() => {
+			job.firstChild.click();
+			setTimeout(() => {
+				const but = document.querySelector('[data-qa="vacancy-response-letter-toggle"]');
+				but.click();
+				setTimeout(() => {
+					const letter = document.querySelector(
+						'[data-qa="vacancy-response-popup-form-letter-input"]'
+					);
+					letter.innerHTML += "Добрый день! Я начинающий frontend разработчик и написал этот скрипт для автоматической отправки откликов, этим я надеюсь привлечь ваше внимание. Скрипт я разместил в моем GitHub профиле. Ссылка на git есть в моем резюме";
+					setTimeout(() => {
+						const finish = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
+						finish.click();
+						
+					}, 1000);
+				}, 1000);
+			}, 1000);
+
+		}, delay);
+
+		delay += 5000;
+	})
+
+})();
