cdae10294113b784aab44972d5fc2d6b3080b1cd
Irving78rus <isakov911@mail.ru>
2023-02-16 02:31:40 +0300

    fix bugs with letter

--- HHScript.js
+++ HHScript.js
@@ -1,28 +1,31 @@
 (() => {
-	let delay = 0;
-	const jobs = document.querySelectorAll('[data-qa="vacancy-serp__vacancy_response"]');
-	jobs.forEach(job => {
-		setTimeout(() => {
-			job.firstChild.click();
-			setTimeout(() => {
-				const but = document.querySelector('[data-qa="vacancy-response-letter-toggle"]');
-				but.click();
-				setTimeout(() => {
-					const letter = document.querySelector(
-						'[data-qa="vacancy-response-popup-form-letter-input"]'
-					);
-					letter.innerHTML += "Добрый день! Я начинающий frontend разработчик и написал этот скрипт для автоматической отправки откликов, этим я надеюсь привлечь ваше внимание. Скрипт я разместил в моем GitHub профиле. Ссылка на git есть в моем резюме";
-					setTimeout(() => {
-						const finish = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
-						finish.click();
-						
-					}, 1000);
-				}, 1000);
-			}, 1000);
+  let delay = 0;
+  const jobs = document.querySelectorAll('[data-qa="vacancy-serp__vacancy_response"]');
+  jobs.forEach(job => {
+  setTimeout(() => {
+    job.firstChild.click();
+    setTimeout(() => {
+      const but = document.querySelector('[data-qa="vacancy-response-letter-toggle"]');
+      but.click();
+      setTimeout(() => {
+        const letter = document.querySelector(
+          '[data-qa="vacancy-response-popup-form-letter-input"]'
+        );
 
-		}, delay);
+        letter.focus();
+        window.dispatchEvent(new KeyboardEvent("keydown", { key: "c" }));
+        letter.dispatchEvent(new KeyboardEvent("keydown", { key: "c" }));
 
-		delay += 5000;
-	})
+        letter.innerHTML +=
+          "Добрый день! Я начинающий frontend разработчик. Я написал этот скрипт для автоматической отправки откликов, этим я надеюсь привлечь ваше внимание. Скрипт я разместил в моем GitHub профиле. Ссылка на git есть в моем резюме";
+        setTimeout(() => {
+          const finish = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
+          finish.click();
+        }, 3000);
+      }, 3000);
+    }, 3000);
+  }, delay);
 
+  delay += 10000;
+})
 })();
