45d95f641588fb86823530f6b3e8145409e157ca
Irving78rus <isakov911@mail.ru>
2023-02-16 15:44:39 +0300

    update code

--- HHScript.js
+++ HHScript.js
@@ -1,31 +1,31 @@
 (() => {
   let delay = 0;
   const jobs = document.querySelectorAll('[data-qa="vacancy-serp__vacancy_response"]');
-  jobs.forEach(job => {
-  setTimeout(() => {
-    job.firstChild.click();
+  jobs.forEach((job) => {
     setTimeout(() => {
-      const but = document.querySelector('[data-qa="vacancy-response-letter-toggle"]');
-      but.click();
+      job.firstChild.click();
       setTimeout(() => {
-        const letter = document.querySelector(
-          '[data-qa="vacancy-response-popup-form-letter-input"]'
-        );
-
-        letter.focus();
-        window.dispatchEvent(new KeyboardEvent("keydown", { key: "c" }));
-        letter.dispatchEvent(new KeyboardEvent("keydown", { key: "c" }));
-
-        letter.innerHTML +=
-          "Добрый день! Я начинающий frontend разработчик. Я написал этот скрипт для автоматической отправки откликов, этим я надеюсь привлечь ваше внимание. Скрипт я разместил в моем GitHub профиле. Ссылка на git есть в моем резюме";
+        const but = document.querySelector('[data-qa="vacancy-response-letter-toggle"]');
+        but.click();
         setTimeout(() => {
-          const finish = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
-          finish.click();
-        }, 3000);
-      }, 3000);
-    }, 3000);
-  }, delay);
+          const letter = document.querySelector(
+            '[data-qa="vacancy-response-popup-form-letter-input"]'
+          );
 
-  delay += 10000;
-})
+          letter.focus();
+          window.dispatchEvent(new KeyboardEvent("keydown", { code: "с" }));
+          letter.dispatchEvent(new KeyboardEvent("keydown", { code: "с" }));
+          letter.value +=
+            "Добрый день! Я начинающий frontend разработчик. Я написал этот скрипт для автоматической отправки откликов, этим я надеюсь привлечь ваше внимание. Скрипт я разместил в моем GitHub профиле. Ссылка на git есть в моем резюме";
+
+          setTimeout(() => {
+            const finish = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
+             finish.click();
+          }, 2000);
+        }, 2000);
+      }, 2000);
+    }, delay);
+
+    delay += 10000;
+  });
 })();
